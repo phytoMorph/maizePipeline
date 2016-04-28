da5b7ed436e9b4acc07ebd0148745cc24fae54bd
poohjh0515 <poohjh0515@gmail.com>
2016-04-28 09:38:47 -0500

    Minor changes made.

--- mecka.m
+++ mecka.m
@@ -19,64 +19,64 @@ function [] = mecka(algorithm,fileName,numberOfObjects,oPath,toSave,toDisplay,sc
                 rawImage_scaleFactor:   A desired percentage to resize the image. If no input, default is 1, noresizing.
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     %}
-    if nargin ~= 6
-        % set to default value of 1
-        rawImage_scaleFactor = 1;
-    end
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    % set to default value of 1200
-    defaultResolution = 1200;
-    scanResolution = StoN(scanResolution);
-    % compute proportion of resolution over default
-    fracDpi = scanResolution/defaultResolution;
-    % set to default value of .25
-    checkBlue_scaleFactor = .25;
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    switch algorithm
-        case 'e'
-            % set to default value of 10
-            CHUNK = 10;
-            % set to default value of 10^6
-            defaultAreaPix = 10^6;
-            defaultAreaPix = round(defaultAreaPix/fracDpi);
-            % set to default value of 100
-            addcut = 100;
-            addcut = round(addcut/fracDpi);
-            % set to default value of 600
-            baselineBlue = 600;
-            baselineBlue = round(baselineBlue/fracDpi);
-            % set to default value of 50
-            fill = 31;
-            [KernelLength sM] = singleEarImage(fileName,numberOfObjects,oPath,rawImage_scaleFactor,checkBlue_scaleFactor,defaultAreaPix,addcut,baselineBlue,fill,CHUNK,toSave,toDisplay)
-            %[KernelLength sM] = singleEarImage(I800,3,oPut,1,.25,1000000,100/2,600,31,10,1,1)
-        case 'c'
-            % set to default value of 10^6
-            defaultAreaPix = 10^6;
-            defaultAreaPix = round(defaultAreaPix/fracDpi);
-            % set to default value of 300
-            rho = 300;
-            rho = round(rho/fracDpi);
-            % set to default value of 100
-            addcut = 100;
-            addcut = round(addcut/fracDpi);
-            % set to default value of 600
-            baselineBlue = 600;
-            baselineBlue = round(baselineBlue/fracDpi);
-            % set to default value of 70
-            colRange1 = 70;
-            % set to default value of 166
-            colRange2 = 166;
-            % set to default value of 50
-            fill = 50;
-            singleCobImage(fileName,numberOfObjects,oPath,rawImage_scaleFactor,checkBlue_scaleFactor,defaultAreaPix,rho,addcut,baselineBlue,colRange1,colRange2,fill,toSave,toDisplay)
-        case 'k'
-            
+    try
+        % no input of rawImage_scaleFactor is default
+        if nargin ~= 8
+            % set to default value of 1
+            rawImage_scaleFactor = 1;
+        end
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        % set to default value of 1200
+        defaultResolution = 1200;
+        scanResolution = StoN(scanResolution);
+        % compute proportion of resolution over default
+        fracDpi = scanResolution/defaultResolution;
+        % set to default value of .25
+        checkBlue_scaleFactor = .25;
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        switch algorithm
+            case 'e'
+                % set to default value of 10
+                CHUNK = 10;
+                % set to default value of 10^6
+                defaultAreaPix = 10^6;
+                defaultAreaPix = round(defaultAreaPix/fracDpi);
+                % set to default value of 100
+                addcut = 100;
+                addcut = round(addcut/fracDpi);
+                % set to default value of 600
+                baselineBlue = 600;
+                baselineBlue = round(baselineBlue/fracDpi);
+                % set to default value of 50
+                fill = 31;
+                [KernelLength sM] = singleEarImage(fileName,numberOfObjects,oPath,rawImage_scaleFactor,checkBlue_scaleFactor,defaultAreaPix,addcut,baselineBlue,fill,CHUNK,toSave,toDisplay)
+                %[KernelLength sM] = singleEarImage(I800,3,oPut,1,.25,1000000,100/2,600,31,10,1,1)
+            case 'c'
+                % set to default value of 10^6
+                defaultAreaPix = 10^6;
+                defaultAreaPix = round(defaultAreaPix/fracDpi);
+                % set to default value of 300
+                rho = 300;
+                rho = round(rho/fracDpi);
+                % set to default value of 100
+                addcut = 100;
+                addcut = round(addcut/fracDpi);
+                % set to default value of 600
+                baselineBlue = 600;
+                baselineBlue = round(baselineBlue/fracDpi);
+                % set to default value of 70
+                colRange1 = 70;
+                % set to default value of 166
+                colRange2 = 166;
+                % set to default value of 50
+                fill = 50;
+                singleCobImage(fileName,numberOfObjects,oPath,rawImage_scaleFactor,checkBlue_scaleFactor,defaultAreaPix,rho,addcut,baselineBlue,colRange1,colRange2,fill,toSave,toDisplay)
+            case 'k'
+                
+        end
+    catch ME
+        close all;
+        getReport(ME);
+        fprintf(['******error in:mecka.m******\n']);
     end
 end
-
-%{
-April 27 2016
-1. clean main functions.
-2. fill this function
-3. headers
-%}
