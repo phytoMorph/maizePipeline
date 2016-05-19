557892c7ebab72da9550e9e93d9cadd66219af38
poohjh0515 <poohjh0515@gmail.com>
2016-05-19 10:02:49 -0500

    Updates on comment and format

--- maizeSeedling/getMASK_ver0.m
+++ maizeSeedling/getMASK_ver0.m
@@ -1,33 +1,52 @@
 function [MASK] = getMASK_ver0(I)
-    hI = rgb2hsv_fast(I,'single');
-    %I = I(:,:,2);
-    level = graythresh(hI(:,:,2));
-    level = max(level,.2);
-    tmp = hI(:,:,2);
-    [y x] = hist(tmp(:),linspace(0,1,256));
-    y = log(y);
-    y = imfilter(y,fspecial('average',[1 11]),'replicate');
-    fidx = find(y == imdilate(y,strel('disk',20)));
-    yclip = y(fidx(1):fidx(2));
-    xclip = x(fidx(1):fidx(2));
-    [ym,midx] = min(yclip);
-    thresh = xclip(midx);
-    
-    MASK = hI(:,:,2) > level & hI(:,:,1) > .04 & hI(:,:,1) < .2 & hI(:,:,3) < .85; 
-    MASK = hI(:,:,1) > .19 & hI(:,:,1) < .22;
-    MASK = hI(:,:,2) > .18; % OLD
-    MASK = hI(:,:,2) > thresh;
     %{
-    R = regionprops(MASK,'Area','PixelIdxList');
-    [mA midx] = max([R.Area]);
-    MASK = zeros(size(MASK));
-    MASK(R(midx).PixelIdxList) = 1;
-    MASK = imclose(MASK,strel('disk',11,0));
-    MASK = imfill(MASK,'holes');
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    About:      
+                getMASK_ver0.m is main function to . (Inputs are relative to 1200dpi)
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    Dependency: 
+                
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    Variable Definition:
+                I:                      the image in variable to operate on.
+                
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     %}
-    tmp = imclearborder(MASK);
-    MASK = MASK - tmp;
-    %MASK = imfill(MASK,'holes');
-    MASK = bwareaopen(MASK,50);
-    % fill in only the largest obj
+    try
+        hI = rgb2hsv_fast(I,'single');
+        %I = I(:,:,2);
+        level = graythresh(hI(:,:,2));
+        level = max(level,.2);
+        tmp = hI(:,:,2);
+        [y x] = hist(tmp(:),linspace(0,1,256));
+        y = log(y);
+        y = imfilter(y,fspecial('average',[1 11]),'replicate');
+        fidx = find(y == imdilate(y,strel('disk',20)));
+        yclip = y(fidx(1):fidx(2));
+        xclip = x(fidx(1):fidx(2));
+        [ym,midx] = min(yclip);
+        thresh = xclip(midx);
+
+        MASK = hI(:,:,2) > level & hI(:,:,1) > .04 & hI(:,:,1) < .2 & hI(:,:,3) < .85; 
+        MASK = hI(:,:,1) > .19 & hI(:,:,1) < .22;
+        MASK = hI(:,:,2) > .18; % OLD
+        MASK = hI(:,:,2) > thresh;
+        %{
+        R = regionprops(MASK,'Area','PixelIdxList');
+        [mA midx] = max([R.Area]);
+        MASK = zeros(size(MASK));
+        MASK(R(midx).PixelIdxList) = 1;
+        MASK = imclose(MASK,strel('disk',11,0));
+        MASK = imfill(MASK,'holes');
+        %}
+        tmp = imclearborder(MASK);
+        MASK = MASK - tmp;
+        %MASK = imfill(MASK,'holes');
+        MASK = bwareaopen(MASK,50);
+        % fill in only the largest obj
+    catch ME
+        close all;
+        getReport(ME)
+        fprintf(['******error in:getMASK_ver0.m******\n']);
+    end       
 end
